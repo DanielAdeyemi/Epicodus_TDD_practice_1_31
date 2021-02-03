9fbbe4e47c7a35d4c1d6eab24956d5dd80f1d395
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-02 22:42:00 -0800

    DRied tests for rectangle

--- __tests__/rectangle.test.js
+++ __tests__/rectangle.test.js
@@ -1,12 +1,14 @@
 import Rectangle from '../src/js/rectangle.js';
 describe('Rectangle', () => {
+  let rectangle;
+  beforeEach(() => {
+    rectangle = new Rectangle(3, 5);
+  });
   test('Should correctly create a rectangle object using two sides', () => {
-    const rectangle = new Rectangle(3, 5);
     expect(rectangle.side1).toEqual(3);
     expect(rectangle.side2).toEqual(5);
   });
   test('should correctly create a rectangle object using 2 sides', () => {
-    const rectangle = new Rectangle(3, 5);
     expect(rectangle.getArea()).toEqual(15);
   });
 });
